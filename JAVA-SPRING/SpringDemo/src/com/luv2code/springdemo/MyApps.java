982715a77f09a1dad6d74d9c8d11b014b497b2f3
JOY-THINKPAD\dutta <raka.dhar@gmail.com>
2021-03-02 22:46:15 -0500

    no message

--- JAVA-SPRING/SpringDemo/src/com/luv2code/springdemo/MyApps.java
+++ JAVA-SPRING/SpringDemo/src/com/luv2code/springdemo/MyApps.java
@@ -5,12 +5,11 @@ public class MyApps {
 	public static void main(String[] args) {	
 		
 		//create the object 
-		BaseballCoach theCoach = new BaseballCoach();
+		Coach theCoach = new TrackCoach();
 		//use the object
         System.out.println(theCoach.getDailyWorkOut());
-	}
+	  }
 
-
-	}
 
 }
+
