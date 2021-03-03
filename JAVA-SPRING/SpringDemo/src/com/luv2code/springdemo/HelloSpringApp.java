53da2289a81a6aa4be0e944f6cf9776acaf1bc09
JOY-THINKPAD\dutta <raka.dhar@gmail.com>
2021-03-02 23:28:37 -0500

    //load the spring configuration file(it can be either xml(legacy way) or java annotation)   //retrieve bean from spring container   //call methods on the bean   //close the context

---
+++ JAVA-SPRING/SpringDemo/src/com/luv2code/springdemo/HelloSpringApp.java
@@ -0,0 +1,22 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+
+	public static void main(String[] args) {
+		
+		//load the spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext2.xml");
+		
+		//retrieve bean from spring container		
+		Coach theCoach = context.getBean("myCoach",Coach.class);
+		
+		//call methods on the bean		
+		System.out.println(theCoach.getDailyWorkOut());
+		
+		//close the context
+          context.close();
+	}
+
+}
--- JAVA-SPRING/SpringDemo/src/com/luv2code/springdemo/MyApps.java
+++ JAVA-SPRING/SpringDemo/src/com/luv2code/springdemo/MyApps.java
@@ -5,7 +5,9 @@ public class MyApps {
 	public static void main(String[] args) {	
 		
 		//create the object 
-		Coach theCoach = new TrackCoach();
+		//BaseballCoach theCoach = new BaseballCoach(); //before interface
+		//Coach theCoach = new BaseballCoach(); //after interface
+		  Coach theCoach = new TrackCoach();
 		//use the object
         System.out.println(theCoach.getDailyWorkOut());
 	  }
